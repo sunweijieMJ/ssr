5bee26fa22d658555a92f23f691f88338191057a
孙伟杰 <sunweijie@sunweijiedeMacBook-Pro.local>
2018-08-28 09:52:33 +0800

    删除多余

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,15 +1,8 @@
 // 将types.js里的内容存为types对象
 import * as types from './types';
 import dynamic from '../api/dynamic';
-import LifeApi from '../api/life/Life';
 
 const actions = {
-  async getCommentsList({ commit, state }, id) {
-    commit('CHANGE_LOADING', true);
-    await LifeApi().getCommentsList({ entity_id: id, entity_type: 6, page: ++state.pageInfo.current_page }).then(res => {
-      if (res.status) commit('COMMENT_LIST', res.data);
-    });
-  },
   getcommonDynamic: ({commit}) => {
     return dynamic.getDynamicById(123).then((res) => {
       commit('UPDATE_DYNAMIC', res.data);
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,24 +2,6 @@
 import * as types from './types';
 
 const mutations = {
-  CHANGE_LOADING: (state, res) => {
-    state.loadInfo.loading = res;
-  },
-  COMMENT_LIST: (state, res) => {
-    state.pageInfo.page_total = res.page_total;
-    if (state.comment_list) {
-      state.comment_list = state.comment_list.concat(res.data);
-    } else {
-      state.comment_list = res.data;
-    }
-    // 触底判断
-    state.loadInfo.loading = false;
-    if (state.pageInfo.current_page >= state.pageInfo.page_total || !state.comment_list.length) {
-      state.loadInfo.loading = true;
-      state.loadInfo.noMore = true;
-    }
-  },
-
   UPDATE_DYNAMIC: (state, data) => {
     state.dynamic = {...data};
   },
