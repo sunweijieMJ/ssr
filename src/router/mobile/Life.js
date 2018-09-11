d6a45aa3a22718fdae5f874cee74528b195b32d5
孙伟杰 <sunweijie@sunweijiedeMacBook-Pro.local>
2018-09-11 15:57:25 +0800

    地图隐藏

--- src/router/mobile/Life.js
+++ src/router/mobile/Life.js
@@ -51,12 +51,14 @@ export default {
       name: 'ActivityShow',
       component: resolve => require(['@/views/mobile/Life/activity/ActivityShow'], resolve)
     },
-    // 地图
-    {
-      path: '/activity_map',
-      name: 'ActivityMap',
-      component: resolve => require(['@/views/mobile/Life/activity/AMap'], resolve)
-    },
+    /*
+     * // 活动地图
+     * {
+     *   path: '/activity_map',
+     *   name: 'ActivityMap',
+     *   component: resolve => require(['@/views/mobile/Life/activity/AMap'], resolve)
+     * },
+     */
     {
       path: 'Personal',
       component: resolve => require(['@/views/mobile/Life/Personal'], resolve),
