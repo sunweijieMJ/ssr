efaf1d00f73406c96687391e01d7901f3f783c9e
孙伟杰 <sunweijie@sunweijiedeMacBook-Pro.local>
2018-12-01 12:25:08 +0800

    test

--- src/views/mobile/personal/ReceiveMember.vue
+++ src/views/mobile/personal/ReceiveMember.vue
@@ -209,6 +209,8 @@ export default {
               alert('注册成功')
               this.$router.push({name: 'ResultPage', query: {status: this.status}});
               // }
+            }).catch(() => {
+              alert('catch');
             });
           }else if(this.status === 2 || this.status === 3 || this.status === 4 || this.status === 5 || this.status === 6 || this.status === 7){
             this.$router.push({name: 'ResultPage', query: {status: this.status}});
@@ -217,7 +219,7 @@ export default {
           }
         });
         // setTimeout(() => {
-          
+
         // }, 1000);
       }
     },
