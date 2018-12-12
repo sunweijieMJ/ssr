66a6a6debd01f7ee9f73bb20615da942564f1c7b
孙伟杰 <sunweijie@sunweijiedeMacBook-Pro.local>
2018-12-12 16:27:34 +0800

    test

--- src/views/mobile/Mall/exhibit/ExhibitDownload.vue
+++ src/views/mobile/Mall/exhibit/ExhibitDownload.vue
@@ -30,7 +30,7 @@
     },
     mounted() {
       if(!(os().isWechat || os().isQQ) && os().isAndroid) {
-        console.log(1);
+        alert(1);
         window.location.href = 'lanehub://myhome/homepage?id=1';
         // window.location.href = `lanehub://myhome/exhibit_list?${linsign.urlConcat(this.$route.query)}`;
       }
