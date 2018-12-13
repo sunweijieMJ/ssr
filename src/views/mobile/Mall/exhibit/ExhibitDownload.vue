d86a734bc48b56b571c6cab787dae41dc5903279
孙伟杰 <sunweijie@sunweijiedeMacBook-Pro.local>
2018-12-13 11:43:17 +0800

    测试

--- src/views/mobile/Mall/exhibit/ExhibitDownload.vue
+++ src/views/mobile/Mall/exhibit/ExhibitDownload.vue
@@ -30,26 +30,29 @@
     },
     mounted() {
       // if((os().isWechat || os().isQQ) || !os().isAndroid) return;
-      this.$el.querySelector('a').click();
+
+      // window.location.href = 'lanehub://myhome/homepage?id=1';
 
       // window.location.href = `lanehub://myhome/exhibit_list?${linsign.urlConcat(this.$route.query)}`;
     },
     methods: {
       schemeUrl(url){
         const ifr = document.createElement('iframe');
-        ifr.src = url;
-        ifr.style.display = 'none';
+        ifr.setAttribute('src', url);
+        ifr.setAttribute('style', 'display:none');
         document.body.appendChild(ifr);
       },
       downApp() {
-        this.schemeUrl('lanehub://myhome/homepage?id=1');
-        // if(os().isiPhone) {
-        //   window.location.href = 'https://a.app.qq.com/o/simple.jsp?pkgname=com.weihe.myhome';
-        // } else if((os().isWechat || os().isQQ) && os().isAndroid) {
-        //   this.popup = true;
-        // } else if(os().isAndroid) {
-        //   window.location.href = 'https://download.lanehub.cn/android?channel=a2';
-        // }
+        if(os().isiPhone) {
+          window.location.href = 'https://a.app.qq.com/o/simple.jsp?pkgname=com.weihe.myhome';
+        } else if((os().isWechat || os().isQQ) && os().isAndroid) {
+          this.popup = true;
+        } else if(os().isAndroid) {
+          window.location.href = `lanehub://myhome/exhibit_list?${linsign.urlConcat(this.$route.query)}`;
+          setTimeout(() => {
+            window.location.href = 'https://download.lanehub.cn/android?channel=a2';
+          }, 1500);
+        }
       }
     }
   };
