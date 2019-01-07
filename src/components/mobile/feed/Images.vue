6e7d9306fd0c8790d834deb3d682d0e4b5b66411
孙伟杰 <sunweijie@sunweijiedeMacBook-Pro.local>
2019-01-07 15:14:32 +0800

    feed

---
+++ src/components/mobile/feed/Images.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="feed-images">
+    <img v-for="(val, i) in images" :key="i" :src="val" alt="">
+  </div>
+</template>
+<script>
+  export default {
+    props: ['images']
+  };
+</script>
+<style lang="scss" scoped>
+  @import '../../../assets/scss/_base.scss';
+
+  .feed-images {
+    padding: 0 0.3rem;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    img {
+      width: 2.2rem;
+      height: 2.2rem;
+      margin-bottom: 0.15rem;
+      border-radius: 0.04rem;
+    }
+  }
+</style>
+
---
+++ src/components/mobile/feed/index.js
@@ -0,0 +1,5 @@
+import Images from './Images.vue';
+
+export {
+  Images
+};
