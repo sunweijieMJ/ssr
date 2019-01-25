c92f42b0985428287b79c67d7c733a94c7535bc8
孙伟杰 <sunweijie@sunweijiedeMacBook-Pro.local>
2019-01-25 21:19:18 +0800

    init

--- src/utils/business/init.js
+++ src/utils/business/init.js
@@ -78,7 +78,8 @@ const couponInit = () => {
       if (!couponBox.length) return;
       for (let i = 0, LEN = couponBox.length; i < LEN; i++) {
         const coupon_id = couponBox[i].getAttribute('data-id');
-        couponBox[i].querySelector('.send-coupon .receive').addEventListener('click', () => {
+        console.log(couponBox[i].querySelector('.send-coupon .receive'));
+        couponBox[i].querySelector('.send-coupon').addEventListener('click', () => {
           window.LaneHub.receiveCoupon(coupon_id);
         }, false);
       }
