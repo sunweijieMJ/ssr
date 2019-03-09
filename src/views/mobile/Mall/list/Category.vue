cf1efd2ca27e19bf2ed4f6789cca62c2d2aca344
元 <liuyuanyuan@weiheinc.com>
2019-03-09 15:16:19 +0800

    二级类目

--- src/views/mobile/Mall/list/Category.vue
+++ src/views/mobile/Mall/list/Category.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mall-category">
     <mall-search>
-      <a :href="global_data.mall.category_banner[0].link" class="banner" v-if="global_data && global_data.mall && global_data.mall.category_banner && global_data.mall.category_banner.length">
+      <!-- <a :href="global_data.mall.category_banner[0].link" class="banner" v-if="global_data && global_data.mall && global_data.mall.category_banner && global_data.mall.category_banner.length">
         <img v-lazy="imageSize(global_data.mall.category_banner[0].img, '690x0')" alt="">
         <p>
           <span>到访体验店 发现更多惊喜</span>
@@ -10,7 +10,35 @@
       </a>
       <ul class="category">
         <li v-for="(item, index) in category.children" :key="index" @click="queryAssign('shop_list', {id: item.obj.id})">{{item.obj.name.slice(0,3)}}</li>
-      </ul>
+      </ul> -->
+      <div class="secondary">
+        <!-- 类目 -->
+        <div class="seond-left">
+          <ul>
+            <li v-for="(a, index) in categray" :class="(active === index) ? 'active' : 'disable'" :key="index" @click="changeTab(index)">{{a.name}}</li>
+          </ul>
+        </div>
+        <div class="second-right">
+          <!-- 热门区 -->
+          <ul class="recommend" v-if="active === 0">
+            <li class="re-li" v-for="(img, mindex) in 10" :key="mindex">
+              <img src="https://imgx.mediav.com/rimg.png?size=112x112&resizetype=3&quality=100&axisreset=1&img=https%3A%2F%2Fp17.ssl.qhimgs3.com%2Fdr%2F240_240_%2Ft01fa0f1f240a37f475.jpg%3Ft%3D1544004883" alt="">
+            </li>
+          </ul>
+          <!-- 分类区 -->
+          <div class="pro-cate" v-else>
+            <div class="pro-banner">
+              <img src="https://imgx.mediav.com/rimg.png?size=112x112&resizetype=3&quality=100&axisreset=1&img=https%3A%2F%2Fp17.ssl.qhimgs3.com%2Fdr%2F240_240_%2Ft01fa0f1f240a37f475.jpg%3Ft%3D1544004883" alt="">
+            </div>
+            <div class="pro-number">
+              <div v-for="(c, cindex) in 5" :key="cindex">
+                <img src="https://imgx.mediav.com/rimg.png?size=112x112&resizetype=3&quality=100&axisreset=1&img=https%3A%2F%2Fp17.ssl.qhimgs3.com%2Fdr%2F240_240_%2Ft01fa0f1f240a37f475.jpg%3Ft%3D1544004883" alt="">
+                <p>三人撒发</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </mall-search>
   </div>
 </template>
@@ -42,8 +70,50 @@
     mixins: [frequent],
     data() {
       return {
-        imageSize
+        imageSize,
+        categray: [
+          {
+            name: '沙发',
+            id: 1
+          },
+          {
+            name: '桌椅',
+            id: 2
+          },
+          {
+            name: '以利萨拜',
+            id: 3
+          },
+
+          {
+            name: '各地阿斯顿',
+            id: 4
+          },
+          {
+            name: '快到拉萨看到吗',
+            id: 5
+          },
+          {
+            name: '沙扣扣开门发',
+            id: 6
+          },
+          {
+            name: '；登陆；审批单',
+            id: 7
+          },
+          {
+            name: '偶滴送滴',
+            id: 8
+          }
+        ],
+        active: +this.$route.query.active ? +this.$route.query.active : 0
       };
+    },
+    methods: {
+      changeTab(num){
+        this.active = num;
+        this.$router.replace({name: 'ShopCategory', query: {active: this.active}});
+      }
     },
     mounted() {
       let that = this;
@@ -95,23 +165,111 @@
         }
       }
     }
-    .category {
+    // .category {
+    //   display: flex;
+    //   flex-wrap: wrap;
+    //   padding: 0.1rem 0.5rem;
+    //   justify-content: space-between;
+    //   li {
+    //     display: flex;
+    //     justify-content: center;
+    //     align-items: center;
+    //     width: 3rem;
+    //     height: 0.88rem;
+    //     margin-bottom: 0.5rem;
+    //     @include thin-line(#b6b6b6, 4px);
+    //     font-size: 0.34rem;
+    //     font-weight: 400;
+    //     color: $themeColor;
+    //     background-color: rgba(250,250,250,1);;
+    //   }
+    // }
+    .secondary{
       display: flex;
-      flex-wrap: wrap;
-      padding: 0.1rem 0.5rem;
-      justify-content: space-between;
-      li {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 3rem;
-        height: 0.88rem;
-        margin-bottom: 0.5rem;
-        @include thin-line(#b6b6b6, 4px);
-        font-size: 0.34rem;
-        font-weight: 400;
-        color: $themeColor;
-        background-color: rgba(250,250,250,1);;
+      justify-content: flex-end;
+      .seond-left{
+        width: 1.8rem;
+        height: calc(100vh - 0.89rem);
+        overflow-y: scroll;
+        -webkit-overflow-scrolling: touch;
+        &::-webkit-scrollbar {display:none}
+        border-right: 1px solid #FAFAFA;
+        box-sizing: border-box;
+        // position: fixed;
+        // bottom: 0;
+        // left: 0;
+        ul{
+          li{
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            line-height: 1.2rem;
+            text-align: center;
+            font-size: 0.3rem;
+            font-weight:300;
+            color: #222222;
+            border: 1px solid #FAFAFA;
+            box-sizing: border-box;
+            background: #FAFAFA;
+          }
+          .active{
+            border-left: 0.04rem solid #0072DD;
+            background-color: #FFFFFF;
+          }
+        }
+      }
+      .second-right{
+        width: 5.7rem;
+        height: calc(100vh - 0.89rem);
+        overflow-y: scroll;
+        -webkit-overflow-scrolling: touch;
+        &::-webkit-scrollbar {display:none}
+        // padding: 0.4rem 0.4rem;
+        .recommend{
+          margin: 0.4rem 0.4rem;
+          // width: 100%;
+          .re-li{
+            width: 4.9rem;
+            height: 2.36rem;
+            margin-bottom: 0.2rem;
+            img{
+              width: 100%;
+              height: 2.36rem;
+            }
+          }
+        }
+        .pro-cate{
+          margin: 0.4rem 0.3rem;
+          .pro-banner{
+            width: 5.1rem;
+            height: 1.7rem;
+            img{
+              width: 5.1rem;
+              height: 1.7rem;
+            }
+          }
+          .pro-number{
+            padding: 0.4rem;
+            display: flex;
+            justify-content: space-between;
+            flex-wrap: wrap;
+            div{
+              margin-bottom: 0.56rem;
+              text-align: center;
+              img{
+                width: 1.8rem;
+                height: 1.8rem;
+              }
+              p{
+                margin-top: 0.2rem;
+                font-size: 0.28rem;
+                font-weight:300;
+                color: #222222;
+                line-height:1;
+              }
+            }
+          }
+        }
       }
     }
   }
