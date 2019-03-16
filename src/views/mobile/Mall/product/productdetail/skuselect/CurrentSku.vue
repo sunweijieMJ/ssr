456f016137222a7b1c647f656727ddf2c7698049
元 <liuyuanyuan@weiheinc.com>
2019-03-16 16:59:47 +0800

    商城单页

--- src/views/mobile/Mall/product/productdetail/skuselect/CurrentSku.vue
+++ src/views/mobile/Mall/product/productdetail/skuselect/CurrentSku.vue
@@ -3,14 +3,14 @@
     <img :src="currentSku.length > 0 ? currentSku[0].optionImgs[0] : '' | imageSize('330x330')" alt="">
     <p>
       <i>¥</i>
-      <span v-if="currentSku.length === 1">{{currentSku[0].optionPrice | divide(100)}}</span><br>
+      <span v-if="currentSku.length === 1">{{currentSku[0].optionPrice | divide(100)}}</span>
       <span class="delete" v-if="currentSku.length === 1">
         <i v-if="currentSku.length === 1 && currentSku[0].optionPrice !== currentSku[0].marketPrice" class="del-i">¥</i>
         <del v-if="currentSku.length === 1 && currentSku[0].optionPrice !== currentSku[0].marketPrice">{{currentSku[0].marketPrice | divide(100)}}</del>
       </span>
       <template v-else>
         <span v-if="product_info.optionsMinPrice === product_info.optionsMaxPrice">{{product_info.optionsMinPrice | divide(100)}}</span>
-        <span v-else>{{product_info.optionsMinPrice | divide(100)}}-{{product_info.optionsMaxPrice | divide(100)}}</span><br>
+        <span v-else>{{product_info.optionsMinPrice | divide(100)}}-{{product_info.optionsMaxPrice | divide(100)}}</span>
         <span class="delete" v-if="product_info.marketMinPrice === product_info.marketMaxPrice">
           <i class="del-i">¥</i>
           <del>{{product_info.marketMinPrice | divide(100)}}</del>
